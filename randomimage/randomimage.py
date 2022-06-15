b883c16f1775379e7515e134105bf3675e9155ce
Yam Yammington <85848371+YamYammington@users.noreply.github.com>
2022-06-15 19:33:38 +0200

    v2.1
    Quality of life improvements + better readability

--- randomimage/randomimage.py
+++ randomimage/randomimage.py
@@ -1,24 +1,25 @@
-from os import listdir
+from os import listdir, getcwd
 from os.path import join, isdir
 from random import choice
 from subprocess import call
 from sys import argv
 
-extensions = ("png", "jpg", "jpeg", "jfif", "gif", "PNG", "JPG", "JPEG", "JFIF", "GIF")
+extensions = ("png", "jpg", "jpeg", "jfif", "gif")
 
 
 def search_file(filepath):
-    chosen_file = join(filepath, choice(listdir(filepath)))
-    match chosen_file:
-        case chosen_file if isdir(chosen_file):
-            search_file(chosen_file)
-        case chosen_file if chosen_file.endswith(data.extensions):
-            print(chosen_file)
-            call([chosen_file], shell=True)
-        case _:
-            search_file(filepath)
+    chosen_file = join(filepath, choice(listdir(filepath))).lower()
+    if isdir(chosen_file):
+        search_file(chosen_file)
+    elif chosen_file.endswith(data.extensions):
+        print(chosen_file)
+        call([chosen_file], shell=True)
+   else:
+        search_file(filepath)
 
 
 if __name__ == "__main__":
     folder = argv[1]
+    if not folder:
+        folder = getcwd()
     search_file(folder)
